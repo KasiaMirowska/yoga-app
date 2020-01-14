47cd5a1ee9081a84da169169acdb2cd873cf0d6f
Kasia <kasia@miroartdesign.com>
2020-01-14 14:34:48 -0500

    de

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: 'https://git.heroku.com/enigmatic-eyrie-84648.git/api',
+    API_ENDPOINT: 'https://enigmatic-eyrie-84648.herokuapp.com/api',
     API_KEY: process.env.REACT_APP_API_KEY,
 }
